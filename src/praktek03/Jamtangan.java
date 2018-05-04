f2fdadcf62ff5a2940a36b0b5692735ee507d95a
Nurul Hikmah <34591217+nrllhkmhh@users.noreply.github.com>
2018-05-04 17:21:09 +0800

    buat method cetakInfo()

--- src/praktek03/Jamtangan.java
+++ src/praktek03/Jamtangan.java
@@ -5,4 +5,11 @@ public class Jamtangan {
     String warna;
     String jenis;
     double harga;
+    
+    void cetakInfo(){
+        System.out.println("Merk \t: "+merk+"\n"+
+                "Warna \t: "+warna+"\n"+
+                "Jenis \t: "+jenis+"\n"+
+                "Harga \t: "+harga);
+    }
 }
