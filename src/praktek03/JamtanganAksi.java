61d997f00991d521755c4236df84d1989ae3aa3b
Nurul Hikmah <34591217+nrllhkmhh@users.noreply.github.com>
2018-05-04 17:30:21 +0800

    Instance, buat attribute dan panggil method

--- src/praktek03/JamtanganAksi.java
+++ src/praktek03/JamtanganAksi.java
@@ -1,6 +1,12 @@
 package praktek03;
 public class JamtanganAksi {
     public static void main(String[] args) {
-    
+        Jamtangan j = new Jamtangan();
+        j.merk="DW";
+        j.warna="Hitam";
+        j.jenis="Jam Analog";
+        j.harga=900000;
+        
+        j.cetakInfo();
     }
 }
