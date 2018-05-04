92c01816b1792e15d1d659a451fdfecb48f54d6c
Nurul Hikmah <34591217+nrllhkmhh@users.noreply.github.com>
2018-05-04 17:38:26 +0800

    Menggunakan method set

--- src/praktek03/JamtanganAksi.java
+++ src/praktek03/JamtanganAksi.java
@@ -2,10 +2,11 @@ package praktek03;
 public class JamtanganAksi {
     public static void main(String[] args) {
         Jamtangan j = new Jamtangan();
-        j.merk="DW";
-        j.warna="Hitam";
-        j.jenis="Jam Analog";
-        j.harga=900000;
+
+        j.setMerk("DW");
+        j.setWarna("Hitam");
+        j.setJenis("Jam Analog");
+        j.setHarga(900000);
         
         j.cetakInfo();
     }
