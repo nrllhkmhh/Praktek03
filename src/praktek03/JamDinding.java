7def8a0872e6d6c04d479802559c016c26bd84f4
Nurul Hikmah <34591217+nrllhkmhh@users.noreply.github.com>
2018-05-05 04:45:59 +0800

    Attribute dari method

--- src/praktek03/JamDinding.java
+++ src/praktek03/JamDinding.java
@@ -1,5 +1,9 @@
 package praktek03;
 
 public class JamDinding extends Jamtangan{
-    
+    private String kekuatanJam;
+            
+    void throttle(){
+        System.out.println("TIKTOK...TIKTOK...");
+    }
 }
