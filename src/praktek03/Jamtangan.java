6fb7c55b9a999185daade884199cc17b56374362
Nurul Hikmah <34591217+nrllhkmhh@users.noreply.github.com>
2018-05-04 17:34:03 +0800

    buat Getter and Setter

--- src/praktek03/Jamtangan.java
+++ src/praktek03/Jamtangan.java
@@ -12,4 +12,38 @@ public class Jamtangan {
                 "Jenis \t: "+jenis+"\n"+
                 "Harga \t: "+harga);
     }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
+    
 }
