1fb77dbd47eacc983426be1d84c961aeed7a4ee7
Nurul Hikmah <34591217+nrllhkmhh@users.noreply.github.com>
2018-05-04 17:45:50 +0800

    menggunakan method get

--- src/praktek03/JamtanganAksi.java
+++ src/praktek03/JamtanganAksi.java
@@ -4,10 +4,19 @@ public class JamtanganAksi {
         Jamtangan j = new Jamtangan();
 
         j.setMerk("DW");
-        j.setWarna("Hitam");
+        j.setWarna("Black");
         j.setJenis("Jam Analog");
         j.setHarga(900000);
         
         j.cetakInfo();
+        
+        System.out.println("Merknya \t: ");
+        System.out.println(j.getMerk());
+        System.out.println("Warnanya \t: ");
+        System.out.println(j.getWarna());
+        System.out.println("Jenisnya \t: ");
+        System.out.println(j.getJenis());
+        System.out.println("Harganya \t: ");
+        System.out.println(j.getHarga());
     }
 }
